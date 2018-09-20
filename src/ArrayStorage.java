0886728ebc376121b006ebfa579de7813f99670d
Иван Кухта <kukhtaia@alfastrah.ru>
2018-09-20 18:06:23 +0700

    changed delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -19,10 +19,8 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i] != null) {
-                if (storage[i].toString().equals(uuid)) {
-                    return storage[i];
-                }
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
