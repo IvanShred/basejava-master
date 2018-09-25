6ed2f38d262cda90cc1c0951f0ec723696437c4c
Иван Кухта <kukhtaia@alfastrah.ru>
2018-09-25 08:39:34 +0700

    HomeWork03

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -28,28 +28,6 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public void save(Resume r) {
-        if (getIndex(r.getUuid()) >= 0) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[size] = r;
-            size++;
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index < 0) {
--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -4,23 +4,42 @@ import model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
 
+    @Override
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            int index = -1 - getIndex(r.getUuid())/*Arrays.binarySearch(storage, 0, size, r)*/;
+            for (int i = index; i < size; i++) {
+                storage[i + 1] = storage[i];
+            }
+            storage[index] = r;
+            size++;
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[size - 1] = null;
+            size--;
+        }
+    }
 
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-        Resume[] sortedStorage = getAll();
-        for (int i = size - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if( sortedStorage[j].compareTo(sortedStorage[j+1]) > 0) {
-                    Resume tmp = sortedStorage[j];
-                    sortedStorage[j] = sortedStorage[j + 1];
-                    sortedStorage[j + 1] = tmp;
-                }
-            }
-        }
-        return Arrays.binarySearch(sortedStorage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 }
