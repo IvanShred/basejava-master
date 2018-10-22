56bb4bfd80ef2dddb0300581c2aac8f96d85160a
HammerFall <hammerfall-91@mail.ru>
2018-10-22 17:19:15 +0700

    HomeWork09 - add AbstractPathtorage

---
+++ src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
@@ -0,0 +1,95 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public abstract class AbstractPathStorage extends AbstractStorage<Path> {
+    private Path directory;
+
+    protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    protected AbstractPathStorage(String dir) {
+        directory = Paths.get(dir);
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not directory or is not writable");
+        }
+    }
+    @Override
+    public void clear() {
+        try {
+            Files.list(directory).forEach(this::doDelete);
+        } catch (IOException e) {
+            throw new StorageException("Path delete error", null);
+        }
+    }
+    @Override
+    public int size() {
+        try {
+            return (int) Files.list(directory).count();
+        } catch (IOException e) {
+            throw new StorageException("Path size error", null);
+        }
+    }
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return new File(directory.toFile(), uuid).toPath();
+    }
+    @Override
+    protected void doUpdate(Resume r, Path path) {
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("Path write error", r.getUuid(), e);
+        }
+    }
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.exists(path);
+    }
+    @Override
+    protected void doSave(Resume r, Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create Path ", path.toString(), e);
+        }
+        doUpdate(r, path);
+    }
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("Path read error", path.getFileName().toString(), e);
+        }
+    }
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString());
+        }
+    }
+    @Override
+    protected List<Resume> doCopyAll() {
+        List<Resume> list = new ArrayList<>();
+        try {
+            Files.list(directory).forEach(path -> list.add(doGet(path)));
+        } catch (IOException e) {
+            throw new StorageException("Copy all error", null);
+        }
+        return list;
+    }
+}
