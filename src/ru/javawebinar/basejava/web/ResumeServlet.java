1bb2bc2285abeab0543a9bba40e91c0f2deab234
HammerFall <hammerfall-91@mail.ru>
2018-12-08 17:28:10 +0700

    HomeWork 15 - display resumes in servlet

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,10 +1,16 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.storage.SqlStorage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -14,9 +20,30 @@ public class ResumeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        //response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        PrintWriter writer = response.getWriter();
+        writer.write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        writer.write("<html>");
+        writer.write("<head>");
+        writer.write("<title>Таблица</title>");
+        writer.write("</head>");
+        writer.write("<body>");
+        writer.write("<table border=\"1\">");
+        writer.write("<tr>");
+        writer.write("<td>uuid</td>");
+        writer.write("<td>full_name</td>");
+        writer.write("</tr>");
+        List<Resume> resumes = new SqlStorage("jdbc:postgresql://localhost:5432/resumes", "postgres", "postgres").getAllSorted();
+        for (Resume resume : resumes) {
+            writer.write("<tr>");
+            writer.write("<td>" + resume.getUuid() + "</td>");
+            writer.write("<td>" + resume.getFullName() + "</td>");
+            writer.write("</tr>");
+        }
+        writer.write("</table>");
+        writer.write("</body>");
+
     }
 }
